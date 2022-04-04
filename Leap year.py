8a39b95ebbf8778591e95be51c7cf28678cb03f9
Roobiah <roobiahmurtadho@gmail.com>
2022-04-04 14:24:46 +0100

    Python Practice

--- Leap year.py
+++ Leap year.py
@@ -1,12 +1,27 @@
-year = int(input("Which year do you want to check? "))
-first_test = year % 4
-second_test = year % 100
-third_test = year % 400
-if first_test != 0:
-    print("Not a leap year")
-elif second_test != 0:
-    print("leap year")
-elif third_test == 0:
-    print("leap year")
-else:
-    print("Not a leap year")
+def is_leap(test_year):
+    first_test = test_year % 4
+    second_test = test_year % 100
+    third_test = test_year % 400
+    if first_test != 0:
+        return False
+    elif second_test != 0:
+        return True
+    elif third_test == 0:
+        return True
+    else:
+        return False
+
+
+def days_in_month(test_year, test_month):
+    if test_month > 12 or test_month < 1:
+        return 'Invalid Month'
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(test_year) and test_month == 2:
+        return 29
+    return month_days[month - 1]
+
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
